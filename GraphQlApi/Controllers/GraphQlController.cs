fe78a7b3f7da05b0669c6ddb0d301e6983d9d8df
andpoulsen <and.poulsen@gmail.com>
2018-03-26 14:03:38 +0200

    Adding an empty GraphQlController

---
+++ GraphQlApi/Controllers/GraphQlController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace GraphQlApi.Controllers
+{
+    public class GraphQlController : ApiController
+    {
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
